cb3af52a5557048003867505b1d9ad3cc375385f
Gonzalo Albito Méndez <gmendez@tecnosylva.com>
2025-02-10 11:51:25 +0100

    chore: direct return

--- Tennis/TennisGame1.cs
+++ Tennis/TennisGame1.cs
@@ -48,56 +48,64 @@ namespace Tennis
 
         public string GetScore()
         {
-            string score = "";
             var tempScore = 0;
-            if (this.player1.score == this.player2.score)
+            if(this.player1.score == this.player2.score)
             {
                 switch (this.player1.score)
                 {
                     case 0:
-                        score = "Love-All";
+                        return "Love-All";
                         break;
                     case 1:
-                        score = "Fifteen-All";
+                        return "Fifteen-All";
                         break;
                     case 2:
-                        score = "Thirty-All";
+                        return "Thirty-All";
                         break;
                     default:
-                        score = "Deuce";
+                        return "Deuce";
                         break;
 
                 }
             }
-            else if (this.player1.score >= 4 || this.player2.score >= 4)
+            if (this.player1.score >= 4 || this.player2.score >= 4)
             {
-                var minusResult = this.player1.score - this.player2.score;
-                if (minusResult == 1) score = "Advantage "+this.player1.name;
-                else if (minusResult == -1) score = "Advantage "+this.player2.name;
-                else if (minusResult >= 2) score = "Win for "+this.player1.name;
-                else score = "Win for "+this.player2.name;
-            }
-            else
-            {
-                for (var i = 1; i < 3; i++)
+                int minusResult = this.player1.score - this.player2.score;
+                if(minusResult == 1)
                 {
-                    if (i == 1) tempScore = this.player1.score;
-                    else { score += "-"; tempScore = this.player2.score; }
-                    switch (tempScore)
-                    {
-                        case 0:
-                            score += "Love";
-                            break;
-                        case 1:
-                            score += "Fifteen";
-                            break;
-                        case 2:
-                            score += "Thirty";
-                            break;
-                        case 3:
-                            score += "Forty";
-                            break;
-                    }
+                    return "Advantage "+this.player1.name;
+                }
+                if(minusResult == -1)
+                {
+                    return "Advantage "+this.player2.name;
+                }
+                if(minusResult >= 2)
+                {
+                    return "Win for "+this.player1.name;
+                }
+                return "Win for "+this.player2.name;
+            }
+
+            string score;
+            for (var i = 1; i < 3; i++)
+            {
+                //TODO Requires to do in two times
+                if (i == 1) tempScore = this.player1.score;
+                else { score += "-"; tempScore = this.player2.score; }
+                switch (tempScore)
+                {
+                    case 0:
+                        score += "Love";
+                        break;
+                    case 1:
+                        score += "Fifteen";
+                        break;
+                    case 2:
+                        score += "Thirty";
+                        break;
+                    case 3:
+                        score += "Forty";
+                        break;
                 }
             }
             return score;
